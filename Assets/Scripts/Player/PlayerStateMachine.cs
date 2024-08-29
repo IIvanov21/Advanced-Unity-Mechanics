e449f28d9b1199dfb1be5f858799623c24915b29
iivanov21 <iivanov21@hotmail.com>
2024-08-29 12:26:56 +0100

    Implemented Player State Scripts

---
+++ Assets/Scripts/Player/PlayerStateMachine.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStateMachine : StateMachine
+{
+    [SerializeField]
+    public InputReader InputReader{  get; private set; }
+
+    private void Start()
+    {
+        SwitchState(new PlayerTestState(this));
+    }
+}
