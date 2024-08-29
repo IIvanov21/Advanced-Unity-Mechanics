d3e6044278291c3ef9ddd7e06c09ccc5d2ccae0e
iivanov21 <iivanov21@hotmail.com>
2024-08-29 14:35:48 +0100

    Environment Added + Player Free State Base

---
+++ Assets/Scripts/Player/PlayerFreeLookState.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFreeLookState : PlayerBaseState
+{
+    /*
+     * Constructor that passes the PlayerStateMachine reference to the base class.
+     */
+    public PlayerFreeLookState(PlayerStateMachine stateMachine) : base(stateMachine) { }
+
+    public override void Enter()
+    {
+    }
+
+    public override void Exit()
+    {
+    }
+
+    public override void Tick(float deltaTime)
+    {
+    }
+}
